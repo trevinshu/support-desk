c33e97c7b5aa6f1ee083820e711e735426681ae2
Trevin Shu <trevinshu2008@gmail.com>
2022-09-30 00:19:17 -0600

    finished section 20: Notes functionality and deploy

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,13 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getTicket, reset, closeTicket } from '../features/tickets/ticketSlice';
+import { getTicket, closeTicket } from '../features/tickets/ticketSlice';
+import { FaPlus } from 'react-icons/fa';
 import { useParams, useNavigate } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import { toast } from 'react-toastify';
 import Spinner from '../components/Spinner';
+import { getNotes, createNote, reset as notesReset } from '../features/notes/noteSlice';
+import NoteItem from '../components/NoteItem';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    width: '600px',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    position: 'relative',
+  },
+};
+
+Modal.setAppElement('#root');
 
 function Ticket() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [noteText, setNoteText] = useState('');
   const { ticket, isLoading, isSuccess, isError, message } = useSelector((state) => state.tickets);
+
+  const { notes, isLoading: notesIsLoading } = useSelector((state) => state.notes);
 
   const params = useParams();
   const navigate = useNavigate();
@@ -20,6 +43,7 @@ function Ticket() {
     }
 
     dispatch(getTicket(ticketId));
+    dispatch(getNotes(ticketId));
     // eslint-disable-next-line
   }, [isError, message, ticketId]);
 
@@ -30,7 +54,22 @@ function Ticket() {
     navigate('/tickets');
   };
 
-  if (isLoading) {
+  //Create note submit
+  const onNoteSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createNote({ noteText, ticketId }));
+    closeModal();
+  };
+
+  //Open/Close Modal
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  if (isLoading || notesIsLoading) {
     return <Spinner />;
   }
 
@@ -52,7 +91,36 @@ function Ticket() {
           <h3>Description of Issue</h3>
           <p>{ticket.description}</p>
         </div>
+        <h2>Notes</h2>
       </header>
+
+      {ticket.status !== 'closed' && (
+        <button className="btn" onClick={openModal}>
+          <FaPlus />
+          Add Note
+        </button>
+      )}
+
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles} contentLabel="Add Note">
+        <h2>Add Note</h2>
+        <button className="btn-close" onClick={closeModal}>
+          X
+        </button>
+        <form onSubmit={onNoteSubmit}>
+          <div className="form-group">
+            <textarea name="noteText" id="noteText" className="form-control" placeholder="Note Text" value={noteText} onChange={(e) => setNoteText(e.target.value)}></textarea>
+          </div>
+          <div className="form-group">
+            <button className="btn" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </Modal>
+
+      {notes.map((note) => (
+        <NoteItem key={note._id} note={note} />
+      ))}
       {ticket.status !== 'closed' && (
         <button className="btn btn-block btn-danger" onClick={onTicketClose}>
           Close Ticket
---
+++ readme.md
@@ -0,0 +1,30 @@
+# Support Desk Application
+
+## Instructor
+
+Brad Traversy
+
+## About
+
+Full Stack (Frontend and Backend) Support Desk Application built with the MERN (MongoDB, Express, React, Node) stack. This is the fourth and final project from the React Front To Back 2022 Course
+
+## Live Link
+
+https://house-marketplace-firebase-react-trevin.vercel.app/
+
+## Languages, Tools, Techniques & Packages Used
+
+- React
+- React Router
+- CRA (Create React App)
+- Redux Toolkit
+- MongoDB
+- Node
+- Express
+- React Icons
+- React Modal
+- React Toastify
+
+## Built By
+
+Trevin Shu
